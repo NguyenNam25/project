3f4770c43d7cb34ffe5d9ec2e7f03c598c8512aa
NguyenNam25 <nqnam102@gmail.com>
2022-10-10 16:38:28 +0700

    delete
    delete

--- project/Home.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "150px";
-  document.getElementById("main-nav").style.marginLeft = "150px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main-nav").style.marginLeft= "0";
-}
